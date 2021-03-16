e89791346e019dafa63b439623701cefce357ba4
Daniel Jo <sungkeun.jo@gmail.com>
2021-03-16 11:47:46 -0700

    Set up HTML templates

--- run.py
+++ run.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Daniel Jo',
+        'place': 'Grand Canyon',
+        'location': 'Arizona',
+        'desc': 'I want to see the Grand Canyon',
+        'created': 'March 16, 2021'
+    },
+    {
+        'author': 'DJ',
+        'place': '63 Building',
+        'location': 'Seoul, Korea',
+        'desc': 'I want to fly and go to Korea',
+        'created': 'March 18, 2021'
+    }
+]
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
