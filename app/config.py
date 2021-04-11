1e2e85098e0661a1b85ae81ab6b5c35539d91f08
Daniel Jo <sungkeun.jo@gmail.com>
2021-04-10 17:26:55 -0700

    Restructured file structure for small application

--- app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SECRET_KEY = '9d53e6ab36c41ded18dae7e637876159'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' 
---
+++ app/routes.py
@@ -0,0 +1,182 @@
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
+from app import app, db, bcrypt
+from app.forms import RegisterForm, LoginForm, UpdateProfileForm, PostForm
+from app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    # posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(
+        Post.created.desc()).paginate(page=page, per_page=5)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(place=form.place.data,
+                    location=form.location.data, desc=form.desc.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created', 'success')
+        return redirect(url_for('home'))
+
+    if current_user.is_authenticated:
+        username = current_user.username
+        image = url_for(
+            'static', filename='profile_pictures/' + current_user.image)
+    else:
+        username = ''
+        image = ''
+
+    return render_template('home.html', posts=posts, form=form, username=username, image=image)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful', 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_image(form_image):
+    random_hex = secrets.token_hex(8)
+    _, file_extension = os.path.splitext(form_image.filename)
+    image_filename = random_hex + file_extension
+    image_path = os.path.join(
+        app.root_path, 'static/profile_pictures/' + image_filename)
+
+    output_size = (125, 125)
+    i = Image.open(form_image)
+    i.thumbnail(output_size)
+    i.save(image_path)
+
+    return image_filename
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.image.data:
+            image_file = save_image(form.image.data)
+            current_user.image = image_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Profile updated successfully', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image = url_for(
+        'static', filename='profile_pictures/' + current_user.image)
+    return render_template('/profile.html', image=image, form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.place = form.place.data
+        post.location = form.location.data
+        post.desc = form.desc.data
+        db.session.commit()
+        flash('Post updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.place.data = post.place
+        form.location.data = post.location
+        form.desc.data = post.desc
+    return render_template('new_post.html', form=form)
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post deleted', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route('/user/<string:username>')
+@login_required
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(
+        Post.created.desc()).paginate(page=page, per_page=5)
+
+    return render_template('user_posts.html', posts=posts, user=user)
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
